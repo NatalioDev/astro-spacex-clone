8e1a907e79905673bf0484aa85c7dc59ecfc3eff
NatalioDev <nataliodg.dev@gmail.com>
2023-11-01 10:33:03 -0300

    astro spacex v1

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react'
+import Sidebar from './Sidebar';
+
+export default function Navigation() {
+  const [open, setOpen] = useState(false)
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+
+    if (prevScrollPos > currentScrollPos) {
+      setVisible(false); // Ocultar la barra de navegación al hacer scroll hacia abajo
+    } else {
+      setVisible(true); // Mostrar la barra de navegación al hacer scroll hacia arriba
+    }
+
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav 
+      id="navbar"
+      className="transition duration-150 bg-transparent absolute m-0 p-0 block top-0 w-screen"
+    >
+    <div className= " lg:max-w-screen-lg my-7 mx-12">
+      <div className="grid lg:flex top-12 lg:left-[50px] w-full justify-items-center h-auto">
+        <a className="relative" href="/">
+          <img src="/spacex-logo-png-transparent2.png" alt="SpaceX Logo" />
+        </a>
+      </div>
+      <div className="flex items-center">
+        <button
+            onClick={() => setOpen(true) }
+            aria-expanded={open}
+            aria-controls="sidebar"
+            aria-label="Open Menu"
+        >
+          <svg 
+              alt="Menu"
+              className="lg:hidden absolute top-8 right-8 w-6 h-6" 
+              xmlns="http://www.w3.org/2000/svg" 
+              fill="none" 
+              viewBox="0 0 24 24" 
+              strokeWidth={1.5} 
+              stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+        </button>
+        <Sidebar open={open} setOpen={setOpen}/>
+        <div className="hidden lg:inline-block lg:left-[485px] lg:text-left pl-[420px] -mt-5">
+          <ul className='relative m-0 p-0'>
+            <li className="hover:text-blue-600 relative inline-block my-0 mx-5 cursor-pointer text-gray-50 text-sm font-bold uppercase opacity-100"><a href="/launches">Launches</a></li>
+            <li className="hover:text-blue-600 relative inline-block my-0 mx-5 cursor-pointer text-gray-50 text-sm font-bold uppercase opacity-100"><a href="/updates">Updates</a></li>
+            <li className="hover:text-blue-600 relative inline-block my-0 mx-5 cursor-pointer text-gray-50 text-sm font-bold uppercase opacity-100"><a href="/mission">Mission</a></li>
+            <li className="hover:text-blue-600 relative inline-block my-0 mx-5 cursor-pointer text-gray-50 text-sm font-bold uppercase opacity-100"><a href="https://shop.spacex.com/">Shop</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+  )
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,77 @@
+import { Transition } from "@headlessui/react";
+
+const Sidebar = ({ open = false, setOpen }) => {
+  return (
+    <>
+      <Transition show={open}>
+        {/* Sliding sidebar */}
+        <Transition.Child
+          className="fixed right-0 top-0 w-[250px] z-30 h-screen"
+          enter="transition ease-in-out duration-300 transform"
+          enterFrom="translate-x-full"
+          enterTo="translate-x-0"
+          leave="transition ease-in-out duration-300 transform"
+          leaveFrom="translate-x-0"
+          leaveTo="translate-x-full"
+        >
+          <div
+            id="sidebar"
+            className={`fixed right-0 top-0 w-[250px] h-screen text-gray-500 bg-black pt-20 p-8 z-50`}
+          >
+            <button
+              onClick={() => setOpen(false)}
+              aria-expanded={open}
+              aria-controls="sidebar"
+            >
+            <svg 
+                alt="Close Menu"
+                xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                strokeWidth={1.5} 
+                stroke="currentColor" 
+                className="absolute top-5 right-5 w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+            </button>
+            <div className="flex flex-col items-end justify-items-end">
+            <div className="hover:text-gray-500 text-white cursor-pointer uppercase text-end w-full">
+                <a  href="/launches">Launches</a>
+                <hr className="my-2 border-gray-800"/>
+              </div>
+              <div className="hover:text-gray-500 text-white cursor-pointer uppercase text-end w-full">
+                <a href="/updates">Updates</a>
+                <hr className="my-2 border-gray-800"/>
+              </div>
+              <div className="hover:text-gray-500 text-white cursor-pointer uppercase text-end w-full">
+                <a href="/services">Services</a>
+                <hr className="my-2 border-gray-800"/>
+              </div>
+              <div className="hover:text-gray-500 text-white cursor-pointer uppercase text-end w-full">
+                <a href="/contact">Contact</a>
+                <hr className="my-2 border-gray-800"/>
+              </div>
+            </div>
+          </div>
+          {/* ... */}
+        </Transition.Child>
+        <Transition.Child
+          enter="transition-opacity ease-linear duration-100"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity ease-linear duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          {/* ... */}
+          <div
+            onClick={() => setOpen(false)}
+            className={`"w-full h-full fixed bg-black opacity-80 z-10 inset-0`}
+          ></div>
+        </Transition.Child>
+      </Transition>
+    </>
+  );
+};
+
+export default Sidebar;
