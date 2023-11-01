ac76710838568a729fc7db1bef861787d14e6380
NatalioDev <nataliodg.dev@gmail.com>
2023-11-01 11:19:51 -0300

    astro spacex v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://NatalioDev.github.io',
+  site: 'https://nataliodev.github.io',
   base: '/astro-spacex-clone',
   integrations: [tailwind(), react()]
 });
